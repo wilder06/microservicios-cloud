644a1d7f576ae73ed1b4810fed50ae492163533d
USERSAD\wpacpach <wpacpach@emeal.nttdata.com>
2022-07-20 20:36:05 -0500

    Config eureka server and client

---
+++ eureka-service/src/test/java/com/app/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
