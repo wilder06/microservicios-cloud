21e0f0aa9e41901eac3191785bef9b96e1f8c592
USERSAD\wpacpach <wpacpach@emeal.nttdata.com>
2022-07-21 00:32:20 -0500

    config controller user resilience4j

--- user-service/src/main/java/com/app/userservice/controller/UserController.java
+++ user-service/src/main/java/com/app/userservice/controller/UserController.java
@@ -3,7 +3,9 @@ package com.app.userservice.controller;
 import com.app.userservice.dto.Car;
 import com.app.userservice.entity.User;
 import com.app.userservice.service.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -32,9 +34,12 @@ public class UserController {
     public Flux<User> findAllClient() {
         return this.userService.findAllUsers();
     }
+    @CircuitBreaker(name = "carsCB", fallbackMethod = "fallBackGetCars")
     @GetMapping("/cars/{idUser}")
     public Mono<Car> getCar(@PathVariable Integer idUser) {
         return this.userService.getCar(idUser);
     }
-
+    private ResponseEntity fallBackGetCars(@PathVariable("userId") int userId, RuntimeException e) {
+        return new ResponseEntity("El usuario " + userId + " tiene los coches en el taller", HttpStatus.OK);
+    }
 }
